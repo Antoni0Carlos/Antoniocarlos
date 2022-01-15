801893913142bd83bd2fc587f15caa1ed14dc283
Antônio carlos <antonicar435@gmail.com>
2022-01-15 12:20:22 -0300

    carrossel ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,15 +63,18 @@ modalBtns.forEach((modalBtn, i) => {
 /*==================== PORTFOLIO ====================*/
 let swiper = new Swiper(".portfolio__container", {
     cssMode: true,
+    loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
+    /*
     mousewheel: true,
-    keyboard: true,
+    keyboard: true,*/
   });
 /*==================== TESTIMONIAL ====================*/
 
